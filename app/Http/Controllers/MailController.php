de25a9812758827ba31d3a93c2c82fd5ccd84fca
Tony Keiser <keisertony@gmail.com>
2019-04-24 01:49:55 -0700

    Mail Packages

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -8,18 +8,19 @@ use Illuminate\Support\Facades\Mail;
 
 class MailController extends Controller {
 
-    public function mail(Request $request) {
-        $this->validate($request, [
-            'name' => 'required|string',
-            'email' => 'required|email',
-            'message' => 'required',
-        ]);
+public function mail() {
+    // public function mail(Request $request) {
+        // $this->validate($request, [
+        //     'name' => 'required|string',
+        //     'email' => 'required|email',
+        //     'message' => 'required',
+        // ]);
 
         Mail::to('keisertony@gmail.com')->send(
-            new MessageSubmitted($request)
+            new MessageSubmitted()
         );
 
-        return response()->json(null, 200);
+        // return response()->json(null, 200);
     }
-    
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-Route::post('/send-message', 'MailController@mail');
+Route::get('/send-message', 'MailController@mail');
 
-Route::get('/{any}', function () {
-    return view('app');
-})->where('any', '.*');
+// Route::get('/{any}', function () {
+//     return view('app');
+// })->where('any', '.*');
